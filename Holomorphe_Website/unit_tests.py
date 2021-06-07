435a3260ed63b6b1857d3988664a709e8e41194e
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-06-07 16:56:13 +0200

    UnitTestsWebAutomationHolomorpheWebsite test_generate_html_to_pdf_target_market_worksheet

--- Holomorphe_Website/unit_tests.py
+++ Holomorphe_Website/unit_tests.py
@@ -5,6 +5,145 @@ from selenium import webdriver
 
 
 class UnitTestsWebAutomationHolomorpheWebsite(unittest.TestCase):
+    def test_generate_html_to_pdf_target_market_worksheet(self):
+        print("test_generate_html_to_pdf_target_market_worksheet")
+
+        time.sleep(5)
+
+        warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+
+        time.sleep(5)
+
+        # with Firefox
+        browser = webdriver.Firefox(executable_path='geckodriver.exe')
+
+        time.sleep(5)
+
+        # maximize window
+        browser.maximize_window()
+
+        time.sleep(5)
+
+        # open the report
+        # browser.get('http://127.0.0.1:8000/reporting/target_market_worksheet')
+        browser.get('https://holomorphe.com/reporting/target_market_worksheet')
+
+        time.sleep(5)
+
+        # fill company name
+        company_name = browser.find_element_by_name("company_name")
+        company_name.clear()
+        company_name.send_keys("company_name")
+
+        time.sleep(5)
+
+        # fill share_capital
+        share_capital = browser.find_element_by_name("share_capital")
+        share_capital.clear()
+        share_capital.send_keys("share_capital")
+        time.sleep(5)
+
+        # fill head_office_address
+        head_office_address = browser.find_element_by_name("head_office_address")
+        head_office_address.clear()
+        head_office_address.send_keys("head_office_address")
+
+        time.sleep(5)
+
+        # fill establishment_number
+        establishment_number = browser.find_element_by_name("establishment_number")
+        establishment_number.clear()
+        establishment_number.send_keys("establishment_number")
+
+        time.sleep(5)
+
+        # fill register_of_trade_and_companies
+        register_of_trade_and_companies = browser.find_element_by_name("register_of_trade_and_companies")
+        register_of_trade_and_companies.clear()
+        register_of_trade_and_companies.send_keys("register_of_trade_and_companies")
+        time.sleep(5)
+
+        # fill main_activities
+        main_activities = browser.find_element_by_name("main_activities")
+        main_activities.clear()
+        main_activities.send_keys("main_activities")
+        time.sleep(5)
+
+        # fill activity_number
+        activity_number = browser.find_element_by_name("activity_number")
+        activity_number.clear()
+        activity_number.send_keys("activity_number")
+
+        time.sleep(5)
+
+        # fill intra_community_vat_number
+        intra_community_vat_number = browser.find_element_by_name("intra_community_vat_number")
+        intra_community_vat_number.clear()
+        intra_community_vat_number.send_keys("intra_community_vat_number")
+        time.sleep(5)
+
+        # fill president
+        president = browser.find_element_by_name("president")
+        president.clear()
+        president.send_keys("president")
+        time.sleep(5)
+
+        # fill registration_date
+        registration_date = browser.find_element_by_name("registration_date")
+        registration_date.clear()
+        registration_date.send_keys("registration_date")
+        time.sleep(5)
+
+        # fill concept
+        concept = browser.find_element_by_name("concept")
+        concept.clear()
+        concept.send_keys("concept")
+
+        time.sleep(5)
+
+        # fill concept_use
+        concept_use = browser.find_element_by_name("concept_use")
+        concept_use.clear()
+        concept_use.send_keys("concept_use")
+
+        time.sleep(5)
+
+        # fill projected_demographic
+        projected_demographic = browser.find_element_by_name("projected_demographic")
+        projected_demographic.clear()
+        projected_demographic.send_keys("projected_demographic")
+
+        time.sleep(5)
+
+        # fill recreation_location
+        recreation_location = browser.find_element_by_name("recreation_location")
+        recreation_location.clear()
+        recreation_location.send_keys("recreation_location")
+
+        time.sleep(5)
+
+        # fill shopping_location
+        shopping_location = browser.find_element_by_name("shopping_location")
+        shopping_location.clear()
+        shopping_location.send_keys("shopping_location")
+
+        time.sleep(5)
+
+        # fill residential_location
+        residential_location = browser.find_element_by_name("residential_location")
+        residential_location.clear()
+        residential_location.send_keys("residential_location")
+
+        time.sleep(5)
+
+        # submit
+        submit = browser.find_element_by_name("button_generate_html_to_pdf_target_market_worksheet")
+        submit.click()
+
+        time.sleep(10)
+
+        browser.quit()
+
     def test_generate_html_to_pdf_business_requirement_document(self):
         print("test_generate_html_to_pdf_business_requirement_document")
 
