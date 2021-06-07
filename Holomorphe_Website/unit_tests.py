bee6461f31ed1cdea7166d9cfcf1c5a483f544e3
Jason ALOYAU <jason.aloyau@outlook.fr>
2021-06-07 18:01:22 +0200

    UnitTestsWebAutomationHolomorpheWebsite test_generate_html_to_pdf_fund_sources_and_uses

--- Holomorphe_Website/unit_tests.py
+++ Holomorphe_Website/unit_tests.py
@@ -5,6 +5,201 @@ from selenium import webdriver
 
 
 class UnitTestsWebAutomationHolomorpheWebsite(unittest.TestCase):
+    def test_generate_html_to_pdf_fund_sources_and_uses(self):
+        print("test_generate_html_to_pdf_fund_sources_and_uses")
+
+        time.sleep(5)
+
+        warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+
+        time.sleep(5)
+
+        # with Firefox
+        browser = webdriver.Firefox(executable_path='geckodriver.exe')
+
+        time.sleep(5)
+
+        # maximize window
+        browser.maximize_window()
+
+        time.sleep(5)
+
+        # open the report
+        browser.get('http://127.0.0.1:8000/reporting/fund_sources_and_uses')
+        # browser.get('https://holomorphe.com/reporting/fund_sources_and_uses')
+
+        time.sleep(5)
+
+        # fill company name
+        company_name = browser.find_element_by_name("company_name")
+        company_name.clear()
+        company_name.send_keys("company_name")
+
+        time.sleep(5)
+
+        # fill share_capital
+        share_capital = browser.find_element_by_name("share_capital")
+        share_capital.clear()
+        share_capital.send_keys("share_capital")
+        time.sleep(5)
+
+        # fill head_office_address
+        head_office_address = browser.find_element_by_name("head_office_address")
+        head_office_address.clear()
+        head_office_address.send_keys("head_office_address")
+
+        time.sleep(5)
+
+        # fill establishment_number
+        establishment_number = browser.find_element_by_name("establishment_number")
+        establishment_number.clear()
+        establishment_number.send_keys("establishment_number")
+
+        time.sleep(5)
+
+        # fill register_of_trade_and_companies
+        register_of_trade_and_companies = browser.find_element_by_name("register_of_trade_and_companies")
+        register_of_trade_and_companies.clear()
+        register_of_trade_and_companies.send_keys("register_of_trade_and_companies")
+        time.sleep(5)
+
+        # fill main_activities
+        main_activities = browser.find_element_by_name("main_activities")
+        main_activities.clear()
+        main_activities.send_keys("main_activities")
+        time.sleep(5)
+
+        # fill activity_number
+        activity_number = browser.find_element_by_name("activity_number")
+        activity_number.clear()
+        activity_number.send_keys("activity_number")
+
+        time.sleep(5)
+
+        # fill intra_community_vat_number
+        intra_community_vat_number = browser.find_element_by_name("intra_community_vat_number")
+        intra_community_vat_number.clear()
+        intra_community_vat_number.send_keys("intra_community_vat_number")
+        time.sleep(5)
+
+        # fill president
+        president = browser.find_element_by_name("president")
+        president.clear()
+        president.send_keys("president")
+        time.sleep(5)
+
+        # fill registration_date
+        registration_date = browser.find_element_by_name("registration_date")
+        registration_date.clear()
+        registration_date.send_keys("registration_date")
+        time.sleep(5)
+
+        # fill line_of_credit
+        line_of_credit = browser.find_element_by_name("line_of_credit")
+        line_of_credit.clear()
+        line_of_credit.send_keys("line_of_credit")
+
+        time.sleep(5)
+
+        # fill outside_equity
+        outside_equity = browser.find_element_by_name("outside_equity")
+        outside_equity.clear()
+        outside_equity.send_keys("outside_equity")
+
+        time.sleep(5)
+
+        # fill personal_equity
+        personal_equity = browser.find_element_by_name("personal_equity")
+        personal_equity.clear()
+        personal_equity.send_keys("personal_equity")
+
+        time.sleep(5)
+
+        # fill term_loan
+        term_loan = browser.find_element_by_name("term_loan")
+        term_loan.clear()
+        term_loan.send_keys("term_loan")
+
+        time.sleep(5)
+
+        # fill other
+        other = browser.find_element_by_name("other")
+        other.clear()
+        other.send_keys("other")
+
+        time.sleep(5)
+
+        # fill total_sources
+        total_sources = browser.find_element_by_name("total_sources")
+        total_sources.clear()
+        total_sources.send_keys("total_sources")
+
+        time.sleep(5)
+
+        # fill cash_reserve
+        cash_reserve = browser.find_element_by_name("cash_reserve")
+        cash_reserve.clear()
+        cash_reserve.send_keys("cash_reserve")
+
+        time.sleep(5)
+
+        # fill inventory
+        inventory = browser.find_element_by_name("inventory")
+        inventory.clear()
+        inventory.send_keys("inventory")
+
+        time.sleep(5)
+
+        # fill purchase_building
+        purchase_building = browser.find_element_by_name("purchase_building")
+        purchase_building.clear()
+        purchase_building.send_keys("purchase_building")
+
+        time.sleep(5)
+
+        # fill purchase_equipment
+        purchase_equipment = browser.find_element_by_name("purchase_equipment")
+        purchase_equipment.clear()
+        purchase_equipment.send_keys("purchase_equipment")
+
+        time.sleep(5)
+
+        # fill renovations
+        renovations = browser.find_element_by_name("renovations")
+        renovations.clear()
+        renovations.send_keys("renovations")
+
+        time.sleep(5)
+
+        # fill working_capital
+        working_capital = browser.find_element_by_name("working_capital")
+        working_capital.clear()
+        working_capital.send_keys("working_capital")
+
+        time.sleep(5)
+
+        # fill other
+        other = browser.find_element_by_name("other")
+        other.clear()
+        other.send_keys("other")
+
+        time.sleep(5)
+
+        # fill total_uses
+        total_uses = browser.find_element_by_name("total_uses")
+        total_uses.clear()
+        total_uses.send_keys("total_uses")
+
+        time.sleep(5)
+
+        # submit
+        submit = browser.find_element_by_name("button_generate_html_to_pdf_fund_sources_and_uses")
+        submit.click()
+
+        time.sleep(10)
+
+        browser.quit()
+
     def test_generate_html_to_pdf_marketing_plan(self):
         print("test_generate_html_to_pdf_marketing_plan")
 
